5e17d8604e18fcf30cd1a9ef1cc10e5723685da8
custer galas <custergls@gmil.com>
2023-03-18 12:54:00 +0800

    laravel activity

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<nav class="nav nav-pills nav-justified">
+    <a class="nav-item nav-link active" href="/">Home</a>
+    <a class="nav-item nav-link" href="/about">About</a>
+    <a class="nav-item nav-link" href="contactUs">Contact Us</a>
+  </nav>
+
+
+<h1>THIS IS HOME</h1>
